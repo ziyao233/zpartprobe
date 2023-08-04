0a7e2587f91dfbb067262e4915e61fb3fd39f9db
Yao Zi <ziyao@disroot.org>
2023-08-05 00:10:46 +0800

    Add hello world

---
+++ zpartprobe.c
@@ -0,0 +1,15 @@
+/*
+ *	zpartprobe
+ *	/zpartprobe.c
+ *	By Mozilla Public License Version 2.0
+ *	Copyright (c) 2023 Yao Zi. All rights reserved.
+ */
+
+#include<stdio.h>
+
+int
+main(void)
+{
+	puts("Hello World awa");
+	return 0;
+}
